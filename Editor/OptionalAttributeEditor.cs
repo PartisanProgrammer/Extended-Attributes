9542c5dff8930fe61ef84c413915a38092dda047
Oliver Källerfors <90176781+PartisanProgrammer@users.noreply.github.com>
2024-04-11 15:48:05 +0200

    Added: [Optional] attribute and updated package info

---
+++ Editor/OptionalAttributeEditor.cs
@@ -0,0 +1,33 @@
+using Attributes;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(OptionalAttribute))]
+public class OptionalAttributeEditor : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        // Draw the default property field
+        
+        label.text += " (Optional)";
+        EditorGUI.PropertyField(position, property, label);
+        
+        // Check if the property represents a reference type (i.e., a class or interface)
+        if (property.propertyType == SerializedPropertyType.ObjectReference)
+        {
+            // Retrieve the object value of the property
+            UnityEngine.Object objValue = property.objectReferenceValue;
+
+            // Check if the object value is null
+            if (objValue == null)
+            {
+                // Calculate the position for the "Optional" label
+                Rect labelPosition = new Rect(position.xMax - 75f, position.y, 75f, position.height);
+
+                // Draw the "Optional" label
+                EditorGUI.LabelField(labelPosition, "Optional", EditorStyles.miniLabel);
+            }
+        }
+       
+    }
+}
---
+++ Runtime/OptionalAttribute.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+namespace Attributes
+{
+    public class OptionalAttribute : PropertyAttribute{}
+    
+}
